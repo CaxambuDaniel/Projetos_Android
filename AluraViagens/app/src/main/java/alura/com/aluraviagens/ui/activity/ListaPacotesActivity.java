cc06577582b130bd8f511f77ea2bfa2f96b56506
Daniel Carvalho <32563295+CaxambuDaniel@users.noreply.github.com>
2022-03-06 13:32:33 -0300

    Implementando o Adapter inicial

--- AluraViagens/app/src/main/java/alura/com/aluraviagens/ui/activity/ListaPacotesActivity.java
+++ AluraViagens/app/src/main/java/alura/com/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -3,8 +3,14 @@ package alura.com.aluraviagens.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 import alura.com.aluraviagens.R;
+import alura.com.aluraviagens.dao.PacoteDAO;
+import alura.com.aluraviagens.model.Pacote;
+import alura.com.aluraviagens.ui.adapter.ListaPoacotesAdapter;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -12,5 +18,10 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listView);
+
+        List<Pacote> pacotes = new PacoteDAO().lista();
+
+        listaDePacotes.setAdapter(new ListaPoacotesAdapter(pacotes, this));
     }
 }
---
+++ AluraViagens/app/src/main/java/alura/com/aluraviagens/ui/adapter/ListaPoacotesAdapter.java
@@ -0,0 +1,47 @@
+package alura.com.aluraviagens.ui.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+import alura.com.aluraviagens.R;
+import alura.com.aluraviagens.model.Pacote;
+
+
+public class ListaPoacotesAdapter extends BaseAdapter{
+
+    private final List<Pacote> pacotes;
+    private Context context;
+
+
+    public ListaPoacotesAdapter(List<Pacote> pacotes, Context context){
+        this.pacotes = pacotes;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return pacotes.size();
+    }
+
+    @Override
+    public Pacote getItem(int posicao) {
+        return pacotes.get(posicao);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int posicao, View view, ViewGroup parent) {
+        View viewCriada = LayoutInflater.from(context)
+                .inflate(R.layout.item_pacote, parent, false);
+        return viewCriada;
+    }
+}
